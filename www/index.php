a02a1ad805ad3ee666122df0002b98bcb7b0b1a9
Samuel Adler <masko552@gmail.com>
2021-05-06 22:56:52 +0200

    secondCommitSadler
    indexUpdatedWithStyle

--- www/index.php
+++ www/index.php
@@ -4,18 +4,38 @@ include "partials/header.php";
 
 ?>
 
-<main>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1>Ahoj</h1>
-      </div>
+<div class="container">
+    <div class="card"></div>
+    <div class="card">
+        <h1 class="title">Examify STU</h1>
+        <form>
+            <div class="input-container">
+                <input type="text" id="#{label}" required="required"/>
+                <label for="#{label}">Meno</label>
+                <div class="bar"></div>
+            </div>
+            <div class="input-container">
+                <input type="text" id="#{label}" required="required"/>
+                <label for="#{label}">Priezvisko</label>
+                <div class="bar"></div>
+            </div>
+            <div class="input-container">
+                <input type="text" id="#{label}" required="required"/>
+                <label for="#{label}">Kód testu</label>
+                <div class="bar"></div>
+            </div>
+            <div class="button-container">
+                <button><span>Spustiť test</span></button>
+            </div>
+            <div class="footer"><a href="#">Ste učiteľ ?</a></div>
+        </form>
     </div>
-  </div>
-</main>
+</div>
 
 <?php
 
 include "partials/footer.php";
 
 ?>
+
+
--- www/partials/header.php
+++ www/partials/header.php
@@ -3,12 +3,13 @@
 
 <head>
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/bootstrap-icons.css">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/styles.css">
-    <title></title>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <title>Finálne zadanie</title>
 </head>
 <body>
