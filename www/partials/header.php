ed19c5c5cce9a0041e6e0ff2b4ce23ae614d9f01
Jakub Rosina <rosina.jakub@gmail.com>
2021-05-06 23:03:41 +0200

    Fix

--- www/partials/header.php
+++ www/partials/header.php
@@ -6,10 +6,11 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/bootstrap-icons.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
 
     <title>Finálne zadanie</title>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 </head>
 <body>
