6f8effc0811612e824c736d49465c08aa10344a9
Samuel Adler <masko552@gmail.com>
2021-05-07 23:20:19 +0200

    changeTeacherLogin
    changeTeacherLogin

--- www/admin.php
+++ www/admin.php
@@ -1,38 +1,27 @@
 <?php
     session_start();
+    if(!empty($_SESSION["loggedin"]))
+    {
+        /*
+        echo $_SESSION["firstName"]."<br>";
+        echo $_SESSION["id"];
+        */
+    }
     include "partials/header.php";
     include "config/config.php";
 
     $link = new mysqli(servername, username, password, database);
     $teacherEmail = $teacherPassword = "";
 
-
-    if(isset($_POST['loginTeacherBtn']))
-    {
-        $teacherEmail = $_POST['loginEmailTeacher'];
-        $teacherPassword = $_POST['loginPasswordTeacher'];
-        $selectData = $link->query("SELECT id, name, surname, password FROM user WHERE email = '$teacherEmail'");
-        $selectedData = mysqli_fetch_assoc($selectData);
-
-        $dbId = $selectedData['id'];
-        $dbPassword = $selectedData['password'];
-        $dbName = $selectedData['name'];
-        $dbSurname = $selectedData['surname'];
-
-        if(password_verify($teacherPassword, $dbPassword))
-        {
-            $_SESSION["id"] = $dbId;
-            $_SESSION["firstName"] = $dbName;
-            $_SESSION["lastName"] = $dbSurname;
-
-            echo '<div id="showModal2" class="modal fade text-center">
+/*
+echo '<div id="showModal2" class="modal fade text-center">
 	            <div class="modal-dialog modal-confirm text-center">
 		            <div class="modal-content text-center">
 			            <div class="modal-header text-center">
 				            <div class="icon-box">
 					            <i class="bi bi-check2"></i>
-				            </div>				
-				            <h4 class="modal-title text-center">Vynikajúco !</h4>	
+				            </div>
+				            <h4 class="modal-title text-center">Vynikajúco !</h4>
 			            </div>
 			        <div class="modal-body text-center">
 				        <p class="text-center">Prihlásenie prebehlo úspešne !</p>
@@ -43,8 +32,8 @@
 		        </div>
 	           </div>
             </div>';
-        }
-    }
+*/
+
 
 ?>
 
--- www/teacher.php
+++ www/teacher.php
@@ -1,14 +1,52 @@
 <?php
 
-include "partials/header.php";
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+{
+    header("location: admin.php");
+    exit;
+}
 
+include "partials/header.php";
+include "config/config.php";
+
+$link = new mysqli(servername, username, password, database);
+$teacherEmail = $teacherPassword = "";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $teacherEmail = $_POST['loginEmailTeacher'];
+    $teacherPassword = $_POST['loginPasswordTeacher'];
+    $selectData = $link->query("SELECT id, name, surname, password FROM user WHERE email = '$teacherEmail'");
+    $selectedData = mysqli_fetch_assoc($selectData);
+
+    $dbId = $selectedData['id'];
+    $dbPassword = $selectedData['password'];
+    $dbName = $selectedData['name'];
+    $dbSurname = $selectedData['surname'];
+
+    if(password_verify($teacherPassword, $dbPassword))
+    {
+        $_SESSION["id"] = $dbId;
+        $_SESSION["firstName"] = $dbName;
+        $_SESSION["lastName"] = $dbSurname;
+        $_SESSION["loggedin"] = true;
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+        {
+            header("location: admin.php");
+            exit;
+        }
+
+    }
+}
 ?>
 
 <div class="container theContainer">
     <div class="card"></div>
     <div class="card">
         <h1 class="title">Examify STU | Učiteľ <br> Prihlásenie</h1>
-        <form action="admin.php" method="post" enctype="multipart/form-data">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="input-container">
                 <input type="email" id="loginEmailTeacher" required="required" name="loginEmailTeacher"/>
                 <label for="loginEmailTeacher">E-mail</label>
