c7dcd2e7b4d19782db91fe564095d0c93a26513c
Dávid Zabák <32141607+Ezzekieqt@users.noreply.github.com>
2021-05-08 19:59:58 +0200

    Update student.php
    -added checkCookie, getCookie, delete_cookie and createCookie functions
    -time of test is now stored in cookies and value always starts on last cookie value

--- www/student.php
+++ www/student.php
@@ -1,11 +1,10 @@
 <?php
-if (!isset($_SESSION)) {
-    session_start();
-}
+session_start();
 
 if(!isset($_SESSION['testCode'])){
     header("location: index.php");
 }
+
 
 include "partials/header.php";
 include "queries/queries.php";
@@ -19,16 +18,64 @@ $time = $row['total_time'];
 ?>
 
 <script type="text/javascript">
+    function checkCookie() {
+        // Get cookie using our custom function
+        var f = getCookie("timer");
+
+        return f !== null;
+    }
+
+    function getCookie(name) {
+        // Split cookie string and get all individual name=value pairs in an array
+        var cookieArr = document.cookie.split(";");
+
+        // Loop through the array elements
+        for(var i = 0; i < cookieArr.length; i++) {
+            var cookiePair = cookieArr[i].split("=");
+
+            /* Removing whitespace at the beginning of the cookie name
+            and compare it with the given string */
+            if(name === cookiePair[0].trim()) {
+                // Decode the cookie value and return
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+
+        // Return null if not found
+        return null;
+    }
+
+    var delete_cookie = function(name) {
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
+
+
+
+
+    function createCookie(name, value) {
+        var date = new Date();
+        date.setTime(date.getTime() + (30*1000));
+        var expires = "; expires= " + date.toGMTString();
+
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+
     var iTime = <?php echo $time; ?>;
+    if(checkCookie('timer')){
+        var iTime = getCookie('timer');
+    }
+
     function countdown()
     {
+        //var inTestTimeExpire = new Date(new Date().getTime() +  <?php echo $time ?> * 1000);
         var i = setInterval(function(){
+            document.cookie = "timer=" + encodeURIComponent(iTime);
             document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTime;
             if(iTime===0){
                 alert('Cas na test vyprsal!');
+                delete_cookie('timer');
                 clearInterval(i);
-                <?php
-                session_destroy(); ?>
                 location.reload();
             } else {
                 iTime--;
@@ -36,6 +83,7 @@ $time = $row['total_time'];
         },1000);
     }
     countdown();
+    <?php session_destroy(); ?>
 </script>
 
     <div id="fixedTimer" class="fancy"></div>
