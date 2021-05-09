2802da4f069bd34cafc39dc840b1b3a06669b684
Samuel Adler <masko552@gmail.com>
2021-05-09 16:17:24 +0200

    timerUpdate
    timerUpdate

--- www/student.php
+++ www/student.php
@@ -17,74 +17,116 @@ $time = $row['total_time'];
 
 ?>
 
-<script type="text/javascript">
-    function checkCookie() {
-        // Get cookie using our custom function
-        var f = getCookie("timer");
-
-        return f !== null;
-    }
-
-    function getCookie(name) {
-        // Split cookie string and get all individual name=value pairs in an array
-        var cookieArr = document.cookie.split(";");
-
-        // Loop through the array elements
-        for(var i = 0; i < cookieArr.length; i++) {
-            var cookiePair = cookieArr[i].split("=");
-
-            /* Removing whitespace at the beginning of the cookie name
-            and compare it with the given string */
-            if(name === cookiePair[0].trim()) {
-                // Decode the cookie value and return
-                return decodeURIComponent(cookiePair[1]);
-            }
+    <script type="text/javascript">
+        function checkCookie()
+        {
+            var f = getCookie("timerMinutes");
+            var g = getCookie("timerSeconds");
+            return f !== null && g !== null;
         }
 
-        // Return null if not found
-        return null;
-    }
+        function getCookie(name) {
+            // Split cookie string and get all individual name=value pairs in an array
+            var cookieArr = document.cookie.split(";");
 
-    var delete_cookie = function(name) {
-        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    };
+            // Loop through the array elements
+            for(var i = 0; i < cookieArr.length; i++) {
+                var cookiePair = cookieArr[i].split("=");
 
-
-
-
-    function createCookie(name, value) {
-        var date = new Date();
-        date.setTime(date.getTime() + (30*1000));
-        var expires = "; expires= " + date.toGMTString();
-
-        document.cookie = name + "=" + value + expires + "; path=/";
-    }
-
-
-    var iTime = <?php echo $time; ?>;
-    if(checkCookie('timer')){
-        var iTime = getCookie('timer');
-    }
-
-    function countdown()
-    {
-        //var inTestTimeExpire = new Date(new Date().getTime() +  <?php echo $time ?> * 1000);
-        var i = setInterval(function(){
-            document.cookie = "timer=" + encodeURIComponent(iTime);
-            document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTime;
-            if(iTime===0){
-                alert('Cas na test vyprsal!');
-                delete_cookie('timer');
-                clearInterval(i);
-                location.reload();
-            } else {
-                iTime--;
+                /* Removing whitespace at the beginning of the cookie name
+                and compare it with the given string */
+                if(name === cookiePair[0].trim()) {
+                    // Decode the cookie value and return
+                    return decodeURIComponent(cookiePair[1]);
+                }
             }
-        },1000);
-    }
-    countdown();
-    <?php session_destroy(); ?>
-</script>
+
+            // Return null if not found
+            return null;
+        }
+
+        var delete_cookie = function(name) {
+            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        };
+
+
+        function createCookie(name, value) {
+            var date = new Date();
+            date.setTime(date.getTime() + (30*1000));
+            var expires = "; expires= " + date.toGMTString();
+
+            document.cookie = name + "=" + value + expires + "; path=/";
+        }
+
+
+        var iTimeMinutes = <?php echo $time; ?>;
+        var iTimeSeconds = iTimeMinutes % 60;
+
+        if(checkCookie('timerMinutes') && checkCookie("timerSeconds"))
+        {
+            iTimeMinutes = getCookie('timerMinutes');
+            iTimeSeconds = getCookie('timerSeconds');
+        }
+
+        function countdown()
+        {
+            console.log("typeMinuty1: ", typeof iTimeMinutes);
+            console.log("typeSekundy1: ", typeof iTimeSeconds);
+
+            console.log("Minuty: ", iTimeMinutes);
+            console.log("Sekundy: ", iTimeSeconds);
+
+            var i = setInterval(function()
+            {
+                document.cookie = "timerMinutes=" + encodeURIComponent(iTimeMinutes);
+                document.cookie = "timerSeconds=" + encodeURIComponent(iTimeSeconds);
+                console.log("typeMinuty1: ", typeof iTimeMinutes);
+                console.log("typeSekundy1: ", typeof iTimeSeconds);
+                if(iTimeMinutes < 10 && iTimeSeconds < 10)
+                {
+                    document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + "0" + iTimeMinutes + ":" + "0" + iTimeSeconds;
+                }
+                else
+                {
+                    if(iTimeMinutes < 10)
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + "0" + iTimeMinutes + ":" + iTimeSeconds;
+                    }
+                    else if(iTimeSeconds < 10)
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTimeMinutes + ":" + "0" + iTimeSeconds;
+                    }
+                    else
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTimeMinutes + ":" + iTimeSeconds;
+                    }
+
+                }
+
+                if(iTimeMinutes === 0 && iTimeSeconds === 0)
+                {
+                    alert('Cas na test vyprsal!');
+                    delete_cookie('timerMinutes');
+                    delete_cookie('timerSeconds');
+                    clearInterval(i);
+                    location.reload();
+                }
+                else
+                {
+                    if(iTimeSeconds === 0)
+                    {
+                        iTimeMinutes--;
+                        iTimeSeconds = 60;
+                    }
+                    iTimeSeconds--;
+
+                }
+            },1000);
+        }
+
+        countdown();
+        <?php session_destroy(); ?>
+    </script>
 
     <div id="fixedTimer" class="fancy"></div>
 
