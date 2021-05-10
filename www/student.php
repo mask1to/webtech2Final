515176d0dc0f19e0f35e7231c6070078744b9bc0
Dávid Zabák <32141607+Ezzekieqt@users.noreply.github.com>
2021-05-10 10:54:37 +0200

    fix
    -timer fix

--- www/student.php
+++ www/student.php
@@ -5,89 +5,203 @@ if(!isset($_SESSION['testCode'])){
     header("location: index.php");
 }
 
-
 include "partials/header.php";
 include "queries/queries.php";
 include "config/config.php";
 
-$link = new mysqli(servername, username, password, database);
+$sessionTestCode = $_SESSION['testCode'];
 
+$link = new mysqli(servername, username, password, database);
 $row = mysqli_fetch_assoc(getTestTime($link,$_SESSION['testCode']));
 $time = $row['total_time'];
 
-?>
+$selectTestCode = $link->query("SELECT id, test_code, total_points FROM test WHERE test_code = '$sessionTestCode'");
+$selectedData = mysqli_fetch_assoc($selectTestCode);
 
-<script type="text/javascript">
-    function checkCookie() {
-        // Get cookie using our custom function
-        var f = getCookie("timer");
+//test id for entered test code
+$testId = $selectedData['id'];
 
-        return f !== null;
-    }
 
-    function getCookie(name) {
-        // Split cookie string and get all individual name=value pairs in an array
-        var cookieArr = document.cookie.split(";");
+$selectTypeOfQuestion = $link->query("SELECT * FROM question WHERE test_id = $testId");
 
-        // Loop through the array elements
-        for(var i = 0; i < cookieArr.length; i++) {
-            var cookiePair = cookieArr[i].split("=");
+if($sessionTestCode == $selectedData['test_code'])
+{
+    echo '<div class="wrapper bg-white rounded">
+            <div class="content">
+            <p class="text-muted"><b>Kód testu: '.$sessionTestCode. '</b></p>
+            <p class="text-muted"><b>Počet bodov v teste: '.$selectedData["total_points"].'</b></p>
+            <div class="text-muted" id="fixedTimer"><b>Zostávajúci čas: 00:00</b></div> <hr>';
 
-            /* Removing whitespace at the beginning of the cookie name
-            and compare it with the given string */
-            if(name === cookiePair[0].trim()) {
-                // Decode the cookie value and return
-                return decodeURIComponent(cookiePair[1]);
+    while($questions = $selectTypeOfQuestion->fetch_assoc())
+    {
+        $questionId = $questions['id'];
+
+        $selectOptions = $link->query("SELECT * FROM questionOption WHERE question_id = '$questionId'");
+        if($questions['type'] == 'checkbox')
+        {
+            echo '<p class="text-muted"><b>Otázka s možnosťami</b></p>
+                   <p class="text-muted""><b>Body: '.$questions['total_points'].'</b></p>
+                   <p class="text-justify h5 pb-2 font-weight-bold">'.$questions['name'].'</p>';
+            while($option = $selectOptions->fetch_assoc())
+            {
+                if($option['question_id'] == $questionId)
+                {
+                    echo '<div class="options py-3"> 
+                     <label class="rounded p-2 option"> '.$option['name'].'
+                     <input type="checkbox" name="radio">
+                     <span class="crossmark"></span>
+                     </label>
+                     ';
+                    echo '</div>';
+                }
             }
+            echo '<hr>';
+        }
+        if($questions['type'] == 'short')
+        {
+            echo '<p class="text-muted"><b>Otázka s krátkou odpoveďou</b></p>
+                   <p class="text-muted""><b>Body: '.$questions['total_points'].'</b></p>
+                   <p class="text-justify h5 pb-2 font-weight-bold">'.$questions['name'].'</p>';
+            while($option = $selectOptions->fetch_assoc())
+            {
+                if($option['question_id'] == $questionId)
+                {
+//                    <label class="rounded p-2 option"> '.$option['name'].'
+//                    <span class="crossmark"></span>
+                    echo '<div class="options py-3">                    
+                     <input type="text">   
+                     </label>
+                     ';
+                    echo '</div>';
+                }
+
+            }
+            echo '<hr>';
         }
 
-        // Return null if not found
-        return null;
     }
-
-    var delete_cookie = function(name) {
-        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    };
+}
 
 
+/*
+echo '
+        <p class="text-muted">Multiple Choice Question</p>
+        <p class="text-justify h5 pb-2 font-weight-bold">What did Radha Krishnan (Cassius Clay at the time) wear while flying to Rome for the 1960 Games?</p>
+        <div class="options py-3"> <label class="rounded p-2 option"> His boxing gloves <input type="radio" name="radio"> <span class="crossmark"></span> </label> <label class="rounded p-2 option"> A parachute <input type="radio" name="radio"> <span class="checkmark"></span> </label> <label class="rounded p-2 option"> Nothing <input type="radio" name="radio"> <span class="crossmark"></span> </label> <label class="rounded p-2 option"> A world little belt <input type="radio" name="radio"> <span class="crossmark"></span> </label> </div> <b>Correct Feedback</b>
+        <p class="mt-2 mb-4 pl-2 text-justify"> Well done! He was scared of flying so picked up the parachute from an support store before the trip. He won gold </p> <b>Incorrect Feedback</b>
+        <p class="my-2 pl-2"> That was incorrect. Try again </p>
+    </div> <input type="submit" value="Add Question" class="mx-sm-0 mx-1">
+</div>';
+*/
 
 
-    function createCookie(name, value) {
-        var date = new Date();
-        date.setTime(date.getTime() + (30*1000));
-        var expires = "; expires= " + date.toGMTString();
+?>
 
-        document.cookie = name + "=" + value + expires + "; path=/";
-    }
+    <script type="text/javascript">
+        function checkCookie()
+        {
+            var f = getCookie("timerMinutes");
+            var g = getCookie("timerSeconds");
+            return f !== null && g !== null;
+        }
 
+        function getCookie(name)
+        {
+            var cookieArr = document.cookie.split(";");
 
-    var iTime = <?php echo $time; ?>;
-    if(checkCookie('timer')){
-        var iTime = getCookie('timer');
-    }
+            for(var i = 0; i < cookieArr.length; i++)
+            {
+                var cookiePair = cookieArr[i].split("=");
 
-    function countdown()
-    {
-        //var inTestTimeExpire = new Date(new Date().getTime() +  <?php echo $time ?> * 1000);
-        var i = setInterval(function(){
-            document.cookie = "timer=" + encodeURIComponent(iTime);
-            document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTime;
-            if(iTime===0){
-                alert('Cas na test vyprsal!');
-                delete_cookie('timer');
-                clearInterval(i);
-                location.reload();
-            } else {
-                iTime--;
+                if(name === cookiePair[0].trim())
+                {
+                    return decodeURIComponent(cookiePair[1]);
+                }
             }
-        },1000);
-    }
-    countdown();
-    <?php session_destroy(); ?>
-</script>
+            return null;
+        }
 
-    <div id="fixedTimer" class="fancy"></div>
+        var delete_cookie = function(name)
+        {
+            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        };
 
+
+        function createCookie(name, value)
+        {
+            var date = new Date();
+            date.setTime(date.getTime() + (30*1000));
+            var expires = "; expires= " + date.toGMTString();
+
+            document.cookie = name + "=" + value + expires + "; path=/";
+        }
+
+
+        //var iTimeMinutes = <?php echo $time; ?>;
+        //var iTimeSeconds = iTimeMinutes % 60;
+        var iTimeMinutes = 4;
+        var iTimeSeconds = 1;
+
+        if(checkCookie('timerMinutes') && checkCookie("timerSeconds"))
+        {
+            iTimeMinutes = getCookie('timerMinutes');
+            iTimeSeconds = getCookie('timerSeconds');
+        }
+
+        function countdown()
+        {
+            var i = setInterval(function()
+            {
+                document.cookie = "timerMinutes=" + encodeURIComponent(iTimeMinutes);
+                document.cookie = "timerSeconds=" + encodeURIComponent(iTimeSeconds);
+
+                if(iTimeMinutes < 10 && iTimeSeconds < 10)
+                {
+                    document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + "0" + iTimeMinutes + ":" + "0" + iTimeSeconds;
+                }
+                else
+                {
+                    if(iTimeMinutes < 10)
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + "0" + iTimeMinutes + ":" + iTimeSeconds;
+                    }
+                    else if(iTimeSeconds < 10)
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTimeMinutes + ":" + "0" + iTimeSeconds;
+                    }
+                    else
+                    {
+                        document.getElementById("fixedTimer").innerHTML = "Zostávajúci čas: " + iTimeMinutes + ":" + iTimeSeconds;
+                    }
+
+                }
+
+                if(iTimeMinutes === 0 && iTimeSeconds === 0)
+                {
+                    alert('Cas na test vyprsal!');
+                    delete_cookie('timerMinutes');
+                    delete_cookie('timerSeconds');
+                    clearInterval(i);
+                    location.reload();
+                }
+                else
+                {
+                    if(iTimeSeconds === 0)
+                    {
+                        iTimeMinutes--;
+                        iTimeSeconds = 60;
+                    }
+                    iTimeSeconds--;
+
+                }
+            },1000);
+        }
+
+        countdown();
+        <?php session_destroy(); ?>
+    </script>
+
+    <!--div id="fixedTimer" class="fancy"></div -->
 
 <?php
 
