5484507a7cb935b161a525f98355cb5004c8abe9
Dávid Zabák <32141607+Ezzekieqt@users.noreply.github.com>
2021-05-11 14:07:48 +0200

    imageNamingFix
    -fixed img name
    - it is now name_surname_testcode

--- www/uploadFile.php
+++ www/uploadFile.php
@@ -1,10 +1,13 @@
 <?php
 
 if(isset($_POST['upload']) && isset($_SESSION['studentName']) && isset($_SESSION['studentSurname']) && isset($_SESSION['testCode'])){
-    $file_name = $_SESSION['studentName']."_".$_SESSION['studentSurname']."_".$_SESSION['testCode']."_";
+    $file_name = $_SESSION['studentName']."_".$_SESSION['studentSurname']."_".$_SESSION['testCode'];
     $file_type = $_FILES['file']['type'];
     $file_size = $_FILES['file']['size'];
-    $file_store = "images/".$file_name.$_FILES['file']['name'];
+
+    $new_name = strtolower(substr($_FILES['file']['name'], strpos($_FILES['file']['name'], '.')));
+
+    $file_store = "images/".$file_name.$new_name;
     $file_tem_loc = $_FILES['file']['tmp_name'];
     move_uploaded_file($file_tem_loc, $file_store);
 }
