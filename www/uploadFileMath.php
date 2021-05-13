e2aa206b315f68a2b35ed7eee41f7075053613ce
Jakub Rosina <rosina.jakub@gmail.com>
2021-05-13 19:53:12 +0200

    Upload minor fix

--- www/uploadFileMath.php
+++ www/uploadFileMath.php
@@ -2,12 +2,12 @@
 session_start();
 //file_put_contents("images/math_questions/".$_SESSION['studentName']."_".$_SESSION['studentSurname']."_".$_SESSION['testCode'].".jpg", $_POST['file']);
 $file_name = $_SESSION['studentName'] . "_" . $_SESSION['studentSurname'] . "_" . $_SESSION['testCode'];
-$_FILES["file-math"]["type"] = "image/jpg";
-$file_type = $_FILES['file-math']['type'];
-$file_size = $_FILES['file-math']['size'];
+$_FILES['file']["type"] = "image/jpg";
+$file_type = $_FILES['file']['type'];
+$file_size = $_FILES['file']['size'];
 
-$new_name = strtolower(substr($_FILES['file-math']['name'], strpos($_FILES['file-math']['name'], '.')));
+$new_name = strtolower(substr($_FILES['file']['name'], strpos($_FILES['file']['name'], '.')));
 
 $file_store = "images/math_questions/" . $file_name . ".jpg";
-$file_tem_loc = $_FILES['file-math']['tmp_name'];
+$file_tem_loc = $_FILES['file']['tmp_name'];
 move_uploaded_file($file_tem_loc, $file_store);
